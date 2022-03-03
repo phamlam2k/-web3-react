fb7b1fd935dcb88d826dd177daa65fe9c9f9afd2
phamlam2k <mailam1309@gmail.com>
2022-03-03 22:26:20 +0700

    finish

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Web3ReactProvider } from '@web3-react/core';
+import { Web3Provider } from '@ethersproject/providers';
+import Web3ReactConnectionComponent from './Web3ReactConnectionComponent';
+window.onload = function() {
+	localStorage.clear();
+};
+
+function App() {
+	const getLibrary = (provider) => {
+		const library = new Web3Provider(provider, 'any');
+		library.pollingInterval = 15000;
+		return library;
+	};
+
+	return (
+		<Web3ReactProvider getLibrary={getLibrary}>
+			<Web3ReactConnectionComponent />
+		</Web3ReactProvider>
+	);
+}
+
+export default App;
